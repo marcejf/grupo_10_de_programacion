3d055027965aca4d695db562ddefc7dfcca8c647
marcela fernandez <marcefernandez0325@gmail.com>
2024-09-27 13:51:10 -0500

    mi primer commit:

---
+++ opeMat.py
@@ -0,0 +1,33 @@
+"""
+un programa con las operaciones basicas y complejas de aritmetica
+"""
+
+#Definir las variables
+num1 = float(input("ingrese un numero"))
+num2 = float(input("ingrese el siguiente numero:"))
+
+#sumar
+suma = num1 + num2 
+print(suma)
+
+#resta
+resta = num1 -num2
+
+#multiplicasion 
+multiplicasion = num1 * num2
+
+#division
+if num2 != 0:
+   division = num1 / num2 
+else: 
+    print("no se puede dividir entre cero")
+
+#potencia
+potencia = num1 ** num2 
+
+#radicacion 
+ 
+#radicacion 
+radicacion = num1 ** (1/num2) 
+
+#print("el resultado de la suma es 
